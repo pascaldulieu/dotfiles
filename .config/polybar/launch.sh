1615d6647a80aa1ae3146e55b45765c0a17ccad7
Pascal Dulieu <spike.dulieu@gmail.com>
2018-12-04 23:30:56 +0000

    Polybar launch script, ranger keybind

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,8 @@
-#!/usr/bin/env sh
-sleep 1;
-# Terminate already running bar instances
-killall polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar
-sleep 1;
-polybar top &
-
-echo "Bars launched..."
+#!/bin/bash
+ps cax | grep polybar > /dev/null
+if [ $? -eq 0 ]; then
+  exit
+else
+  sleep 1;
+  polybar top &
+fi
---
+++ .config/polybar/old-launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+sleep 1;
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+sleep 1;
+polybar top &
+
+echo "Bars launched..."
